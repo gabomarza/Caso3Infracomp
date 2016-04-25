63c0379a0b2de96a83491a7025915ced41613c34
Eduardo <Eduardo@Eduardo-PC.uniandes.edu.co>
2016-04-25 16:40:05 -0500

    Agregue algo del generador de carga

---
+++ src/GeneradorDeCarga/Generador.java
@@ -0,0 +1,40 @@
+package GeneradorDeCarga;
+
+import uniandes.gload.core.LoadGenerator;
+import uniandes.gload.core.Task;
+
+public class Generador 
+{
+	/**
+	 * El generador
+	 */
+	private LoadGenerator generador;
+	
+	/**
+	 * El constructor
+	 */
+	public Generador()
+	{
+		Task tarea = crearTarea();
+		int numTareas = 100;
+		int tiempoEntreTareas = 1000;
+		generador = new LoadGenerator("Prueba", numTareas, tarea, tiempoEntreTareas);
+		generador.generate();
+	}
+	
+	/**
+	 * Creador de tareas
+	 */
+	private Task crearTarea()
+	{
+		return null;
+	}
+	
+	/**
+	 * Main
+	 */
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Generador gen = new Generador();
+	}
+}
